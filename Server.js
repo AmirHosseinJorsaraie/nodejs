5816237a9e9d91111a276a8f71d60318b2f2e7b5
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-11 22:56:05 +0330

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const postsRout = require('./Routs/Server/posts')
+const swaggerDocument = require('./swagger/ServerSwagger.json')
+const swagger = require('swagger-ui-express')
 
 
 app.use('/posts',postsRout)
 
-
+app.use("/", swagger.serve, swagger.setup(swaggerDocument))
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
