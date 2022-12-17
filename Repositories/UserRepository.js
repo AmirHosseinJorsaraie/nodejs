940bb66edc67ab181d9d41806fddd5efa1330298
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-17 21:51:09 +0330

    Update UserRepository.js

--- Repositories/UserRepository.js
+++ Repositories/UserRepository.js
@@ -1,12 +1,23 @@
 const User = require('../Models/User')
+const Role = require('../Repositories/RoleRepository')
 
 var userList = []
 
 GetUsers()
 
-async function AddUser(username, password) {
+async function AddUser(username, password, roles) {
     try {
-        await User.create({ username: username, password: password })
+        var userRoles = []
+        var allRoles = await Role.GetRoleList()
+        roles.forEach((ur)=>{
+            allRoles.forEach((r)=>{
+                if(r.dataValues.id == ur){
+                    userRoles.push(ur);
+                }
+            })
+        })
+        var user = await User.create({ username: username, password: password })
+        await user.addRoles(userRoles)
         UpdateUserList()
         return `Oppration successfully completed!`
     } catch (err) {
@@ -27,16 +38,21 @@ async function UpdateUserList() {
 async function UserVerification(user) {
     let userVerified = false
     let userId
+    let userForFindRoles
     userList.forEach((u) => {
         if (u.dataValues.username == user.username && u.dataValues.password == user.password) {
             userId = u.dataValues.id
             userVerified = true
+            userForFindRoles = u
         }
     })
-
+    
+    
+    let userRoles = await userForFindRoles.getRoles()
+  
     if (!userVerified) return { status: 403, message: 'username or password is incorrect.' }
 
-    return { userId: userId }
+    return { userId: userId , userRoles: userRoles}
 }
 
 module.exports = { AddUser, GetUsers, UserVerification }
