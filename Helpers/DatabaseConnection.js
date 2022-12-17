00808659ed9746d922234a1246371b47be06fdf2
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-17 21:57:46 +0330

    Delete Helpers directory

--- Helpers/DatabaseConnection.js
+++
@@ -1,7 +0,0 @@
-const {Sequelize, Model, DataTypes} = require('sequelize')
-const sequelize = new Sequelize(process.env.DbName,process.env.DbUsername,process.env.DbPassword,{
-    dialect: 'mssql',
-    host: 'localhost'
-})
-
-module.exports = {sequelize,Model,DataTypes}
