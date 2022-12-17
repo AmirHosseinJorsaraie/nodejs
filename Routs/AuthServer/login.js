6cb65ad44c44340fa925d44d39a31b186c26726e
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-17 21:50:31 +0330

    Update login.js

--- Routs/AuthServer/login.js
+++ Routs/AuthServer/login.js
@@ -13,15 +13,20 @@ router.post('/', async (req, res) => {
     var result = await User.UserVerification({ username, password })
 
     if (!result.userId) return res.status(result.status).json(result.message)
+    
+    var userRoles = [];
+    result.userRoles.forEach(roles => {
+        userRoles.push({RoleName:roles.dataValues.RoleName , id: roles.dataValues.RoleName})
+    });
 
-    const accessToken = JWT.sign({ username, password }, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '2m' })
+    const accessToken = JWT.sign({ username, password , userRoles}, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '2m' })
 
     if (await RefreshToken.RefreshTokenExists(result.userId)) {
         var refToken = await RefreshToken.GetRefreshTokenByUserId(result.userId)
         return res.status(200).json({ accessToken: accessToken, refreshToken: refToken })
     }
 
-    const refreshToken = JWT.sign({ username, password }, process.env.REFRESH_SECRET_TOKEN)
+    const refreshToken = JWT.sign({ username, password ,userRoles }, process.env.REFRESH_SECRET_TOKEN)
 
     try {
         await RefreshToken.AddRefreshToken(refreshToken, result.userId)
