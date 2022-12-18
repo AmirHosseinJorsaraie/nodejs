2eb5f44a82dabbc249b74f4e3e4ec2a7b20e8446
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-18 15:55:41 +0330

    Update login.js

--- Routs/AuthServer/login.js
+++ Routs/AuthServer/login.js
@@ -14,19 +14,21 @@ router.post('/', async (req, res) => {
 
     if (!result.userId) return res.status(result.status).json(result.message)
     
-    var userRoles = [];
+    var userPermisions = result.userPermisions 
+    var userRoles = []
     result.userRoles.forEach(roles => {
         userRoles.push({RoleName:roles.dataValues.RoleName , id: roles.dataValues.RoleName})
     });
 
-    const accessToken = JWT.sign({ username, password , userRoles}, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '2m' })
+    const accessToken = JWT.sign({ username, password , userRoles , userPermisions}, process.env.ACCESS_SECRET_TOKEN, { expiresIn: process.env.ACCESS_TOKEN_EXPIER_TIME})
+
 
     if (await RefreshToken.RefreshTokenExists(result.userId)) {
         var refToken = await RefreshToken.GetRefreshTokenByUserId(result.userId)
         return res.status(200).json({ accessToken: accessToken, refreshToken: refToken })
     }
 
-    const refreshToken = JWT.sign({ username, password ,userRoles }, process.env.REFRESH_SECRET_TOKEN)
+    const refreshToken = JWT.sign({ username, password ,userRoles , userPermisions}, process.env.REFRESH_SECRET_TOKEN)
 
     try {
         await RefreshToken.AddRefreshToken(refreshToken, result.userId)
