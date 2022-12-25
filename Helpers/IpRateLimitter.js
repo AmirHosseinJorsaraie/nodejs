28830f9c05267e13524e923e3a10515ea5e65dec
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-25 11:56:30 +0330

    Update IpRateLimitter.js

--- Helpers/IpRateLimitter.js
+++ Helpers/IpRateLimitter.js
@@ -3,17 +3,31 @@ var rateLimiter = []
 function IpRateLimit(req, res, next) {
 
     var ipExists = rateLimiter.find(r => r.ip == req.socket.remoteAddress)
-    console.log(ipExists)
-    var tryCunts = ipExists ? ipExists.tryCunts : 1
 
-    if (tryCunts > 5) return res.status(400).json({ message: 'you cant try for 5 min later!' })
+    var tryCunts = ipExists ? ipExists.tryCunts : 1
     
+    if (tryCunts >= process.env.IpRateLimit) {
+
+        if (!ipExists.blocked) {
+            IpBlackList(ipExists)
+            ipExists.blocked = true
+        }
+        return res.status(400).json({ message: 'you cant try for 1 minute!' })
+    }
+
     if (ipExists) {
-        ipExists.tryCunts++
+        ipExists.tryCunts = tryCunts + 1
         return next()
     }
-    rateLimiter.push({ ip: req.socket.remoteAddress, tryCunts: tryCunts })
+
+    rateLimiter.push({ ip: req.socket.remoteAddress, tryCunts: tryCunts, blocked: false })
     next()
+}
+
+async function IpBlackList(Ip) {
+    setTimeout(() => {
+        rateLimiter = rateLimiter.filter((item) => item.ip == Ip)
+    }, 60000)
 }
 
 module.exports = IpRateLimit
