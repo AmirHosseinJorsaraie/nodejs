05ff7442f931c700a3ce43f9e10b498123b51cef
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-27 22:03:10 +0330

    Update IpRateLimitter.js

--- Helpers/IpRateLimitter.js
+++ Helpers/IpRateLimitter.js
@@ -2,26 +2,45 @@ var rateLimiter = []
 
 function IpRateLimit(req, res, next) {
 
-    var ipExists = rateLimiter.find(r => r.ip == req.socket.remoteAddress)
+   // var ipExists = rateLimiter.find(r => r.ip == req.socket.remoteAddress)
+    redisClient.get(req.socket.remoteAddress, async (error, Ip) => {
 
-    var tryCunts = ipExists ? ipExists.tryCunts : 1
-    
-    if (tryCunts >= process.env.IpRateLimit) {
-
-        if (!ipExists.blocked) {
-            IpBlackList(ipExists)
-            ipExists.blocked = true
+        if (error) console.log(error)
+        if (!Ip) {
+            currentIp = { tryCunts: 1}
+            await redisClient.setEx(req.socket.remoteAddress, process.env.IP_EXPIRE_TIME, JSON.stringify(currentIp))
+            return
         }
-        return res.status(400).json({ message: 'you cant try for 1 minute!' })
-    }
 
-    if (ipExists) {
-        ipExists.tryCunts = tryCunts + 1
-        return next()
-    }
+        let ip = JSON.parse(Ip)
 
-    rateLimiter.push({ ip: req.socket.remoteAddress, tryCunts: tryCunts, blocked: false })
-    next()
+        if (ip.tryCunts >= process.env.IP_RATE_LIMIT) {
+            return res.json({message: 'you are blocked and you have to wait'})
+        }
+
+        ip.tryCunts = tryCunts + 1
+
+        await redisClient.del(req.socket.remoteAddress)
+        await redisClient.setEx(req.socket.remoteAddress, process.env.IP_EXPIRE_TIME, JSON.stringify(ip))
+        next()
+    })
+    // var tryCunts = ipExists ? ipExists.tryCunts : 1
+
+    // if (currentIp.tryCunts >= process.env.IP_RATE_LIMIT) {
+
+    //     if (!ipExists.blocked) {
+    //         IpBlackList(ipExists)
+    //         ipExists.blocked = true
+    //     }
+    //     return res.status(400).json({ message: 'you cant try for 1 minute!' })
+    // }
+
+    // if (ipExists) {
+    //     ipExists.tryCunts = tryCunts + 1
+    //     return next()
+    // }
+
+    // rateLimiter.push({ ip: req.socket.remoteAddress, tryCunts: tryCunts, blocked: false })
 }
 
 async function IpBlackList(Ip) {
