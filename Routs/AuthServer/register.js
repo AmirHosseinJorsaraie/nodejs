77e21e5830d982e407c251343da4b12c56d8ef65
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-30 21:27:41 +0330

    Update register.js

--- Routs/AuthServer/register.js
+++ Routs/AuthServer/register.js
@@ -1,4 +1,4 @@
-const User = require('../../Repositories/UserRepository')
+const User = require('../../Models/User')
 const IpRateLimit = require('../../Helpers/IpRateLimitter') 
 const express = require('express')
 const router = express.Router()
