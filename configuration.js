b802bfc1e4b6918d253efc27b907cc93571a8dc4
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-31 21:47:11 +0330

    Delete configuration.js

--- configuration.js
+++
@@ -1,5 +0,0 @@
-const Permisions = [{PermisionName:'Create Posts'},{PermisionName:'Watch Posts'},{PermisionName:'Delete Posts'}]
-const Roles = [{RoleName: 'Admin',RolePermisions:[1,2,3]},{RoleName: 'RegularUser',RolePermisions:[2]}]
-const Users = [{username: 'Amir',password:'admin',Roles:[1]},{username: 'user',password:'123',Roles:[2]}]
-
-module.exports = {Permisions,Roles,Users}
