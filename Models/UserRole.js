09c9505ed3c157ed1c16f7ef758d8cbe3f0a8bbf
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-31 21:46:17 +0330

    Delete Models directory

--- Models/UserRole.js
+++
@@ -1,9 +0,0 @@
-const User = require('./User')
-const Role = require('./Role')
-const { sequelize, Model, DataTypes } = require('../Helpers/DatabaseConnection')
-
-class UserRole extends Model{}
-
-
-
-module.exports = UserRole
