84e777e06db752739bc25d84c14d5e2b48f4e8f5
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2022-12-31 17:35:28 +0330

    Delete Helpers directory

--- Helpers/DbMigration.js
+++
@@ -1,9 +0,0 @@
-const {sequelize} = require('./DatabaseConnection')
-const User = require('../Models/User')
-const Permision = require('../Models/Permision')
-const RegisterToken = require('../Models/RegisterToken')
-const Role = require('../Models/Role')
-const UserRole = require('../Models/UserRole')
-const RolePermision = require('../Models/RolePermision')
-
-sequelize.sync();
--- Helpers/GetEvents.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events')
-const eventEmitter = new EventEmitter()
-
-eventEmitter.on('server.start',(Port)=>{
-    console.log(`server is listening on port ${Port} ...`)
-})
-
-
-module.exports = eventEmitter
