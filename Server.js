2cdcc2c7666c5110db6fce37ee12e141c763ffca
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2023-01-07 14:50:02 +0330

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,13 +1,38 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const PORT = 3000
-const eventEmitter = require('./Helpers/GetEvents')
-const postsRout = require('./Routs/Server/posts')
-const swaggerDocument = require('./swagger/ServerSwagger.json')
-const swagger = require('swagger-ui-express')
+var cluster = require('cluster');
 
-app.use('/posts',postsRout)
+if (cluster.isMaster) {
+    var numWorkers = require('os').cpus().length;
 
-app.use("/api-doc", swagger.serve, swagger.setup(swaggerDocument))
-app.listen(PORT, () => eventEmitter.emit('server.start',PORT))
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+    for (var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', function (worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+
+} else {
+
+
+    require('dotenv').config()
+    const express = require('express')
+    const app = express()
+    const PORT = 3000
+    const eventEmitter = require('./Helpers/GetEvents')
+    const postsRout = require('./Routs/Server/posts')
+    const swaggerDocument = require('./swagger/ServerSwagger.json')
+    const swagger = require('swagger-ui-express')
+
+    app.use('/posts', postsRout)
+
+    app.use("/api-doc", swagger.serve, swagger.setup(swaggerDocument))
+    app.listen(PORT, () => eventEmitter.emit('server.start', PORT))
+}
