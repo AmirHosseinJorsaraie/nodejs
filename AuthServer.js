35313e8abc4d0be181925f0dc3e82b3c9f9cdbb8
AmirHossein_J <32485488+xxXUNKNOWNXxx@users.noreply.github.com>
2023-01-07 14:49:35 +0330

    Update AuthServer.js

--- AuthServer.js
+++ AuthServer.js
@@ -1,36 +1,60 @@
-require('dotenv').config()
-require('./Helpers/DbConfigModels') //for configuration the sequelize models
-const express = require('express')
-const eventEmitter = require('./Helpers/GetEvents')
-const loginRout = require('./Routs/AuthServer/login')
-const registerRout = require('./Routs/AuthServer/register')
-const refreshRout = require('./Routs/AuthServer/refresh')
-const logoutRout = require('./Routs/AuthServer/logout')
-const addPermision = require('./Routs/AuthServer/Permision/addPermision')
-const addRole = require('./Routs/AuthServer/Role/addRole')
-const posts = require('./Routs/AuthServer/ServerProxyRouts/posts')
-const swaggerDocument = require('./swagger/AuthServerSwagger.json')
-const swagger = require('swagger-ui-express')
-const app = express()
+var cluster = require('cluster');
+
+if(cluster.isMaster) {
+ var numWorkers = require('os').cpus().length;
+
+ console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+ for(var i = 0; i < numWorkers; i++) {
+  cluster.fork();
+ }
+
+ cluster.on('online', function(worker) {
+  console.log('Worker ' + worker.process.pid + ' is online');
+ });
+
+ cluster.on('exit', function(worker, code, signal) {
+  console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+  console.log('Starting a new worker');
+  cluster.fork();
+ });
+ 
+} else {
+    require('dotenv').config()
+    require('./Helpers/DbConfigModels') //for configuration the sequelize models
+    const express = require('express')
+    const eventEmitter = require('./Helpers/GetEvents')
+    const loginRout = require('./Routs/AuthServer/login')
+    const registerRout = require('./Routs/AuthServer/register')
+    const refreshRout = require('./Routs/AuthServer/refresh')
+    const logoutRout = require('./Routs/AuthServer/logout')
+    const addPermision = require('./Routs/AuthServer/Permision/addPermision')
+    const addRole = require('./Routs/AuthServer/Role/addRole')
+    const posts = require('./Routs/AuthServer/ServerProxyRouts/posts')
+    const swaggerDocument = require('./swagger/AuthServerSwagger.json')
+    const swagger = require('swagger-ui-express')
+    const app = express()
 
 
-// (async ()=>{
-//     await _user.AddUser('testUser','@134')
-// })();
+    // (async ()=>{
+    //     await _user.AddUser('testUser','@134')
+    // })();
 
 
-// _user.GetUsers().then((result)=> {
-//     result.forEach((u)=>{
-//         console.log(u.dataValues)
-//     })
-// })
-app.use(express.json())
-app.use('/login',loginRout)
-app.use('/register',registerRout)
-app.use('/refresh',refreshRout)
-app.use('/logout',logoutRout)
-app.use('/addPermision',addPermision)
-app.use('/addRole',addRole)
-app.use('/posts',posts)
-app.use("/api-doc", swagger.serve, swagger.setup(swaggerDocument))
-app.listen(process.env.PORT,()=>eventEmitter.emit('server.start',process.env.PORT))
+    // _user.GetUsers().then((result)=> {
+    //     result.forEach((u)=>{
+    //         console.log(u.dataValues)
+    //     })
+    // })
+    app.use(express.json())
+    app.use('/login', loginRout)
+    app.use('/register', registerRout)
+    app.use('/refresh', refreshRout)
+    app.use('/logout', logoutRout)
+    app.use('/addPermision', addPermision)
+    app.use('/addRole', addRole)
+    app.use('/posts', posts)
+    app.use("/api-doc", swagger.serve, swagger.setup(swaggerDocument))
+    app.listen(process.env.PORT, () => eventEmitter.emit('server.start', process.env.PORT))
+}
+
